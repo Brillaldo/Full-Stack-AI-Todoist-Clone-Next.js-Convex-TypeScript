0a3c306ead16dea7e5262471e5d3e63875aec9e1
Brillaldo <grilloserra@gmail.com>
2024-10-06 15:05:47 -0600

    COnvex

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";import "./globals.css";
+import { ConvexClientProvider } from "./convex-client-provider";
 
 const inter = Inter({subsets : ["latin"]});
 
@@ -18,8 +19,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={inter.className} >
-        {children}
+        className={inter.className} ><ConvexClientProvider>
+        {children} </ConvexClientProvider>
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
+import Tasks from "@/components/todovex/task";
 import { Button } from "@/components/ui/button";
+import Image from "next/image";
 
 export default function Home() {
   return (
@@ -6,6 +8,8 @@ export default function Home() {
       <main className="flex -screen flex-col items-center justify-between p-24">
         <h1>Todovex</h1>
         <Button variant={"outline"}>Hi</Button>
+       
+      <Tasks/>
       </main>
       
     
