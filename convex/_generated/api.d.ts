05a5b520397e0ddcf67460b791cf27225610d42d
Brillaldo <grilloserra@gmail.com>
2024-10-07 14:17:12 -0600

    Bug

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -18,6 +18,7 @@ import type {
 import type * as authAdapter from "../authAdapter.js";
 import type * as http from "../http.js";
 import type * as tasks from "../tasks.js";
+import type * as todos from "../todos.js";
 
 /**
  * A utility for referencing Convex functions in your app's API.
@@ -31,6 +32,7 @@ declare const fullApi: ApiFromModules<{
   authAdapter: typeof authAdapter;
   http: typeof http;
   tasks: typeof tasks;
+  todos: typeof todos;
 }>;
 export declare const api: FilterApi<
   typeof fullApi,
