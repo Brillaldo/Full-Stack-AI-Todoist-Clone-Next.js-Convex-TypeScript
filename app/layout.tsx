dda8c48d68c1a6ba31afe61e33697ac0f52d009a
Brillaldo <grilloserra@gmail.com>
2024-10-13 19:55:36 -0600

    Project Deletion

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,27 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";import "./globals.css";
-import { ConvexClientProvider } from "./provider";
+import { Noto_Sans_Georgian } from "next/font/google";
+import "./globals.css";
 import { Toaster } from "@/components/ui/toaster";
 
-const inter = Inter({subsets : ["latin"]});
+const defaultFont = Noto_Sans_Georgian({ subsets: ["latin"] });
+
+const ORIGIN_URL =
+  process.env.NODE === "production"
+    ? "https://todovex.ai"
+    : "http://localhost:3000";
 
 export const metadata: Metadata = {
   title: "Todovex.ai",
-  description: "TodoVex seamlessly organizes your task and predicts what's nextusing AI.",
-
-}
-
+  description:
+    "TodoVex seamlessly organizes your tasks and predicts what's nextusing AI.",
+  icons: {
+    icon: "/icon.ico",
+  },
+  metadataBase: new URL(ORIGIN_URL),
+  alternates: {
+    canonical: ORIGIN_URL,
+  },
+};
 
 export default function RootLayout({
   children,
@@ -19,12 +30,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={inter.className} >
+      <body className={defaultFont.className}>
         {children}
         <Toaster />
       </body>
-     
     </html>
   );
 }
